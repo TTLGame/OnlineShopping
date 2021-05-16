b9779deb6636298fb16523b4e9b78ae724a84282
Trần Phi Long <72856973+18127141@users.noreply.github.com>
2021-05-16 22:54:29 +0700

    HTML page

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require('http');
+
 const server = http.createServer(function(req,res){
-    res.end("hello word from nodejs http server");
+
+    
+    res.end();
 });
 server.listen(5000,function(){
     console.log('Http server is listening on port 5000');
